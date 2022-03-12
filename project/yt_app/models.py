a6d36eb7dcb4f66053a9234074fb8cc095feef1c
Dawid <zgredzik987@gmail.com>
2022-03-12 10:04:11 +0100

    settings

--- project/yt_app/models.py
+++ project/yt_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Match(models.Model):
+    name = models.CharField(max_length=30)
+    created = models.DateTimeField(auto_now_add=True)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+
+class Games(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    tournaments = models.ManyToManyField(Tournament)
+
+
+class Scores(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    tournaments = models.ManyToManyField(Tournament)
