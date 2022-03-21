af812c14e85a4caddd1d2f38be15fbc2faaf15a0
Dawid <zgredzik987@gmail.com>
2022-03-21 21:55:41 +0100

    some models

--- project/yt_app/models.py
+++ project/yt_app/models.py
@@ -6,6 +6,7 @@ class Tournament(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
+    online = models.BooleanField(default=True)
 
 
 class Match(models.Model):
@@ -14,7 +15,13 @@ class Match(models.Model):
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
 
 
-class Games(models.Model):
+class Games(models.Model):  
+    G_TYPES = (
+        (1, "RTS"),
+        (2, "FPS"),
+        (3, "MOBA")
+    )
+    game_type = models.IntegerField(choices=G_TYPES)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
@@ -25,10 +32,11 @@ class Scores(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
+    name1 = models.CharField(max_length=30)
 
 
 class Place(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
-    created = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=300)
     tournaments = models.ManyToManyField(Tournament)
